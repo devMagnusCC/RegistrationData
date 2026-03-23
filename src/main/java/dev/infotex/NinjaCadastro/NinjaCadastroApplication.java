31359c6855d08494f65162eb5ed9419670cd922a
joao <dev.joaovictor.inftotex@gmail.com>
2026-03-23 13:22:17 -0300

    first commit

---
+++ src/main/java/dev/infotex/NinjaCadastro/NinjaCadastroApplication.java
@@ -0,0 +1,13 @@
+package dev.infotex.NinjaCadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjaCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjaCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/infotex/NinjaCadastro/NinjaCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.infotex.NinjaCadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjaCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
